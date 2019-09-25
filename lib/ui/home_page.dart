43a5e77b1c1820a6a2b68ee10ab0b681f6081828
Willian <reiswn@gmail.com>
2019-09-24 22:18:15 -0300

    share add

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:buscador_de_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -115,7 +116,10 @@ class _HomePageState extends State<HomePage>{
               fit: BoxFit.cover,),
               onTap: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index])));
-              }
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+              },
           );
           else
             return Container(
